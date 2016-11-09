452b3c15762a70f03a6db3cea09a8fb1109cb060
Binu Tomy <binu_tomy@hotmail.co.uk>
2016-11-09 22:29:13 +0000

    updating [Rock paper scissors]

--- Oop/script.js
+++ Oop/script.js
@@ -2,7 +2,10 @@ function RockPaperScissors (){
 
     var options = ["rock", "scissors", "paper"];
     var computerChoice = options[ Math.floor(Math.random() * 3)];
-    var playerChoice = prompt("Do you choose rock, paper or scissors?").toLowerCase();
+    alert('Computer Chooses ' + computerChoice);
+    var playerChoice = document.getElementById("playerChoice").value;
+
+    var output = document.getElementById('output');
 
     var results = {
         'scissors':{'paper':   "you win!",   'rock':    'you lose...'},
@@ -12,8 +15,39 @@ function RockPaperScissors (){
     if(playerChoice == computerChoice){
         alert("It's a tie!");
     }else{
-        alert(results[playerChoice][computerChoice]);
+        output.innerHTML = results[playerChoice][computerChoice];
     }  
+
 };
 
-RockPaperScissors();
+
+// function RockPaperScissors (){ 
+
+//     var choices = ["rock","paper","scissors"];
+//     var computerChoice = choices[ Math.floor(Math.random() * 3)];
+//     alert('Computer Chooses ' + computerChoice);
+//     var playerChoice = document.getElementById("playerChoice").value;
+
+//     var map = {};
+
+//     choices.forEach(function(choice, i) {
+//         map[choice] = {};
+//         for (var j = 0, half = (choices.length-1)/2; j < choices.length; j++) {
+//             var opposition = (i+j)%choices.length
+//             if (!j)
+//                 map[choice][choice] = "Was a tie"
+//             else if (j <= half)
+//                 map[choice][choices[opposition]] = choices[opposition] + " wins"
+//             else
+//                 map[choice][choices[opposition]] = choice + " wins"
+//         }
+//     })
+
+//     function compare(choice1, choice2) {
+//         return (map[choice1] || {})[choice2] || "Invalid choice";
+//         alert((map[choice1] || {})[choice2] || "Invalid choice");
+//     }
+
+// document.body.appendChild(document.createTextNode(compare(playerChoice,computerChoice)));
+
+// };
