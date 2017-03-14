e7c4ab0e665e1d4f7404899b716eba27dacd7858
Binu Tomy <binu_tomy@hotmail.co.uk>
2017-03-14 17:33:56 +0000

    updating js [ lottery draw]

--- Vanilla Js/Task3/script.js
+++ Vanilla Js/Task3/script.js
@@ -1,32 +1,39 @@
 //3. Create a lottery draw app. Add a big “DRAW” button which draws 5 random numbers from 1 - 50. Do an animation of 5 lottery balls rolling in screen after the draw.(2 hours)
 
 
-function randomNumber(){
-	return Math.floor((Math.random() * 51));
-};
 
 
-// function drawClicked() {
-// 	var numbers = document.getElementsByClassName('wrapper')[0].children;
-// 	if (numbers[0].classList == 'animation') {
-// 		console.log('hello');
-// 	} else {
-// 		numbers.className += ' ' + 'animation';
-// 	}
-//
-// };
-
-
-$(document).ready(
-	$( ".draw" ).click(function() {
-		var numbers  = $('.wrapper div');
-		$(numbers).addClass('animation');
-
-		for (var i = 0; i <= 4; i++) {
-		    var drawnumber = numbers[i];
-		    var test = randomNumber();
-		    $(drawnumber).text(test);
-
-		}
+(function (){
+	
+	function drawClicked() {
+		let numbers = document.querySelectorAll(".wrapper div");
+		numbers.forEach ( number => {
+			if(!number.classList.contains("animation")) {
+					number.className += ' ' + 'animation';
+					number.innerHTML = Math.floor((Math.random() * 51));
+			}
+		});
 	}
-));
+	var drawBtn = document.getElementById('draw');
+	drawBtn.addEventListener("click", drawClicked);
+
+})();
+
+
+
+
+
+//
+// $(document).ready(
+// 	$( ".draw" ).click(function() {
+// 		var numbers  = $('.wrapper div');
+// 		$(numbers).addClass('animation');
+//
+// 		for (var i = 0; i <= 4; i++) {
+// 		    var drawnumber = numbers[i];
+// 		    var test = randomNumber();
+// 		    $(drawnumber).text(test);
+//
+// 		}
+// 	}
+// ));
