8c5c167c57a899e1cd3486a041bf8bcf89c02a04
Binu Tomy <binu_tomy@hotmail.co.uk>
2017-03-17 17:26:03 +0000

    updating task 5

---
+++ Task5/script.js
@@ -0,0 +1,43 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'sales.csv', true);
+
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    // Success!
+    var csvData =request.responseText.split(";");
+    var header = null;
+   	var resultData = [];
+   	// var finalD = {};
+
+   	var headerProps = [city, name, ...]
+
+    var dataArray = csvData.forEach(function(item, i) {
+    	var dataObject = {};
+    	var dataRow = item.split(",");
+    	for (var data of dataRow) {
+    		// resultData.push({test[data]});
+    		// resultData.push() 
+    		// header = (i === 0) ? : null;
+
+    		dataObject[headerProps[i]] = data;
+
+    	}
+
+    	// dataObject -> push to resultData   	
+
+    });
+
+
+
+  } else {
+    console.error("Error");
+  }
+};
+
+[{"city ": "london", "date" : date, name : name },{"city ": "london"}]
+
+// request.onerror = function() {
+//   // There was a connection error of some sort
+// };
+
+request.send();
