de0530a0044675f587d81e89a1ef6747af9980bb
Binu Tomy <binu_tomy@hotmail.co.uk>
2017-03-28 22:45:28 +0100

    final changes [new project]

--- project-new/src/js/index.js
+++ project-new/src/js/index.js
@@ -1,5 +1,11 @@
-import Person from "./Person.js";
+(function (){
 
-let john = new Person('John');
+  function showNavigation(e) {
+    e.preventDefault
+    let navBlock = document.querySelector(".nav-list");
+    navBlock.classList.toggle('active');
+  }
+	let navBtn = document.getElementById('nav-button');
+	navBtn.addEventListener("click", showNavigation);
 
-console.log( john.getName() );
+})();
